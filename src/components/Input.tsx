fe35c4da9a0674dd8762d7fdfdf0f5dd42fa835f
kunndnn <44980639+kunndnn@users.noreply.github.com>
2026-05-09 20:46:57 +0530

    feat: add reusable Input component and implement admin login page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,58 +1,44 @@
-const Input = ({
-  label = "",
-  labelClassName = "",
-  errorMessage = "",
-  errorClassName = "",
-  type = "text",
-  value = "",
-  onChange = () => {},
-  placeholder = "",
-  className = "",
-  disabled = false,
-  maxLength = 50,
-}: {
+import React, { forwardRef } from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   labelClassName?: string;
   errorMessage?: string;
   errorClassName?: string;
-  type?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
-  className?: string;
-  disabled?: boolean;
-  maxLength?: number;
-}) => {
-  if (label || errorMessage) {
-    return (
-      <div className="flex flex-col gap-1">
-        {label && <label className={labelClassName}>{label}</label>}
-        <input
-          type={type}
-          value={value}
-          onChange={onChange}
-          placeholder={placeholder}
-          className={className}
-          disabled={disabled}
-          maxLength={maxLength}
-        />
-        {errorMessage && <p className={errorClassName}>{errorMessage}</p>}
-      </div>
-    );
-  }
+}
 
-  // simple input
-  return (
-    <input
-      type={type}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      className={className}
-      disabled={disabled}
-      maxLength={maxLength}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      label = "",
+      labelClassName = "",
+      errorMessage = "",
+      errorClassName = "",
+      className = "",
+      ...props
+    },
+    ref
+  ) => {
+    if (label || errorMessage) {
+      return (
+        <div className="flex flex-col gap-1">
+          {label && <label className={labelClassName}>{label}</label>}
+          <input ref={ref} className={className} {...props} />
+          {errorMessage && (
+            <p className={`text-red-500 text-sm mt-1 ${errorClassName}`}>
+              {errorMessage}
+            </p>
+          )}
+        </div>
+      );
+    }
+
+    // simple input
+    return <input ref={ref} className={className} {...props} />;
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
--- src/pages/admin/auth/Login.tsx
+++ src/pages/admin/auth/Login.tsx
@@ -4,8 +4,11 @@ import Input from "../../../components/Input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, type LoginFormData } from "../../../types/auth.schema";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -18,6 +21,10 @@ const Login = () => {
     console.log(data);
   };
 
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="w-full h-screen bg-[#dbedfb] p-5">
       <div className="bg-white flex items-center h-full shadow rounded-xl overflow-hidden">
@@ -25,11 +32,13 @@ const Login = () => {
           coming soon.
         </div>
 
-        <div className="w-full md:w-1/2 flex-col flex justify-center items-center">
+        <div className="w-full md:w-1/2 flex-col flex justify-center items-center px-4">
           <form
             onSubmit={handleSubmit(onSubmit)}
             className="w-full max-w-md space-y-4"
           >
+            <h2 className="font-bold text-2xl">Welcome Back!</h2>
+            <p className="text-gray-500">Please enter your details to login</p>
             <Input
               label="Email Address"
               type="email"
@@ -37,27 +46,40 @@ const Login = () => {
               className="border w-full p-3 rounded"
               {...register("email")}
               errorMessage={errors.email?.message}
-              errorClassName={
-                errors.email?.message ? "text-red-500 text-sm mt-1" : ""
-              }
             />
             <Input
               label="Password"
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="Enter password"
               className="border w-full p-3 rounded"
               {...register("password")}
               errorMessage={errors.password?.message}
-              errorClassName={
-                errors.password?.message ? "text-red-500 text-sm mt-1" : ""
-              }
             />
+            <Button onClick={togglePassword}>Toggle</Button>
             <Button
               type="submit"
-              className="bg-black text-white px-5 py-3 rounded w-full"
+              className="bg-black text-white px-5 py-3 rounded w-full text-lg"
             >
-              {isSubmitting ? "Loading..." : "Login"}
+              {isSubmitting ? "Loading..." : "Sign In ->"}
             </Button>
+            <p className="text-right">
+              <Link to="/admin/forgot-password" className="text-primary">
+                Forgot password ?
+              </Link>
+            </p>
+            <div className="flex justify-between items-center">
+              <div className="w-full border-t border-gray-500"></div>
+              <div className="px-4 w-full uppercase text-[12px]">
+                or continue with
+              </div>
+              <div className="w-full border-t border-gray-500"></div>
+            </div>
+            <p className="text-gray-500">
+              Don't have an account?{" "}
+              <Link to="/signup" className="text-primary">
+                Sign Up
+              </Link>
+            </p>
           </form>
         </div>
       </div>
